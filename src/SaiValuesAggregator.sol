a2b56f2149cb5789bd7e7381eb43eaac5c87cc90
Gonzalo Balabasquer <gbalabasquer@gmail.com>
2018-10-04 18:34:24 -0300

    Add contracts addrs + test cases

--- src/SaiValuesAggregator.sol
+++ src/SaiValuesAggregator.sol
@@ -17,9 +17,14 @@ pragma solidity ^0.4.24;
 
 import "ds-math/math.sol";
 
+contract TopInterface {
+    function tub() public view returns (TubInterface);
+    function tap() public view returns (TapInterface);
+}
+
 contract TubInterface {
     function vox() public view returns (VoxInterface);
-    function tap() public view returns (TapInterface);
+    function pit() public view returns (address);
     function pip() public view returns (PipInterface);
     function pep() public view returns (PepInterface);
     function mat() public view returns (uint);
@@ -36,7 +41,6 @@ contract TubInterface {
     function rhi() public view returns (uint);
     function off() public view returns (bool);
     function out() public view returns (bool);
-    function pit() public view returns (address);
     function gem() public view returns (TokInterface);
     function gov() public view returns (TokInterface);
     function skr() public view returns (TokInterface);
@@ -67,92 +71,136 @@ contract PepInterface {
 }
 
 contract TokInterface {
+    function totalSupply() public view returns (uint);
     function balanceOf(address) public view returns (uint);
     function allowance(address, address) public view returns (uint);
 }
 
 contract SaiValuesAggregator is DSMath {
+    TopInterface public top;
     TubInterface public tub;
+    TapInterface public tap;
+    VoxInterface public vox;
+    address      public pit;
+    PipInterface public pip;
+    PepInterface public pep;
+    TokInterface public gem;
+    TokInterface public gov;
+    TokInterface public skr;
+    TokInterface public sai;
+    TokInterface public sin;
 
-    constructor(address _tub) public {
-        tub = TubInterface(_tub);
+    constructor(address _top) public {
+        top = TopInterface(_top);
+        tub = top.tub();
+        tap = top.tap();
+        vox = tub.vox();
+        pit = tub.pit();
+        pip = tub.pip();
+        pep = tub.pep();
+        gem = tub.gem();
+        gov = tub.gov();
+        skr = tub.skr();
+        sai = tub.sai();
+        sin = tub.sin();
+    }
+
+    function getContractsAddrs() public view returns (
+                                                        uint blockNumber,
+                                                        address top_,
+                                                        address tub_,
+                                                        address tap_,
+                                                        address vox_,
+                                                        address pit_,
+                                                        address pip_,
+                                                        address pep_,
+                                                        address gem_,
+                                                        address gov_,
+                                                        address skr_,
+                                                        address sai_,
+                                                        address sin_
+                                                    ) {
+        blockNumber = block.number;
+        top_ = top;
+        tub_ = tub;
+        tap_ = tap;
+        vox_ = vox;
+        pit_ = pit;
+        pip_ = pip;
+        pep_ = pep;
+        gem_ = gem;
+        gov_ = gov;
+        skr_ = skr;
+        sai_ = sai;
+        sin_ = sin;
     }
 
     // Return the aggregated values from tub, vox and tap
-    function aggregateValues() public view returns (
+    function aggregateValues(address myAddr, address myProxy) public view returns (
                                                         uint blockNumber,
-                                                        bytes32 pip,
+                                                        bytes32 pipVal,
                                                         bool pipSet,
-                                                        bytes32 pep,
+                                                        bytes32 pepVal,
                                                         bool pepSet,
                                                         bool off,
                                                         bool out,
-                                                        uint[] r
+                                                        uint[] sValues, // System Values
+                                                        uint[] tValues // Token Values
                                                     ) {
         blockNumber = block.number;
 
-        (pip, pipSet) = tub.pip().peek(); // Price feed value for gem
-        (pep, pepSet) = tub.pep().peek(); // Price feed value for gov
+        (pipVal, pipSet) = pip.peek(); // Price feed value for gem
+        (pepVal, pepSet) = pep.peek(); // Price feed value for gov
 
         off = tub.off(); // Cage flag
         out = tub.out(); // Post cage exit
 
-        r = new uint[](17);
+        sValues = new uint[](17);
         // Tub
-        r[0] = tub.axe(); // Liquidation penalty
-        r[1] = tub.mat(); // Liquidation ratio
-        r[2] = tub.cap(); // Debt ceiling
-        r[3] = tub.fit(); // REF per SKR (just before settlement)
-        r[4] = tub.tax(); // Stability fee
-        r[5] = tub.fee(); // Governance fee
-        r[6] = tub.chi(); // Accumulated Tax Rates
-        r[7] = tub.rhi(); // Accumulated Tax + Fee Rates
-        r[8] = tub.rho(); // Time of last drip
-        r[9] = tub.gap(); // Join-Exit Spread
-        r[10] = tub.tag(); // Abstracted collateral price (ref per skr)
-        r[11] = tub.per(); // Wrapper ratio (gem per skr)
+        sValues[0] = tub.axe(); // Liquidation penalty
+        sValues[1] = tub.mat(); // Liquidation ratio
+        sValues[2] = tub.cap(); // Debt ceiling
+        sValues[3] = tub.fit(); // REF per SKR (just before settlement)
+        sValues[4] = tub.tax(); // Stability fee
+        sValues[5] = tub.fee(); // Governance fee
+        sValues[6] = tub.chi(); // Accumulated Tax Rates
+        sValues[7] = tub.rhi(); // Accumulated Tax + Fee Rates
+        sValues[8] = tub.rho(); // Time of last drip
+        sValues[9] = tub.gap(); // Join-Exit Spread
+        sValues[10] = tub.tag(); // Abstracted collateral price (ref per skr)
+        sValues[11] = tub.per(); // Wrapper ratio (gem per skr)
         // Vox
-        r[12] = tub.vox().par(); // Dai Target Price (ref per dai)
-        r[13] = tub.vox().way(); // The holder fee (interest rate)
-        r[14] = tub.vox().era();
+        sValues[12] = vox.par(); // Dai Target Price (ref per dai)
+        sValues[13] = vox.way(); // The holder fee (interest rate)
+        sValues[14] = vox.era();
         // Tap
-        r[15] = tub.tap().fix(); // Cage price
-        r[16] = tub.tap().gap(); // Boom-Bust Spread
-    }
+        sValues[15] = tap.fix(); // Cage price
+        sValues[16] = tap.gap(); // Boom-Bust Spread
 
-    function aggregateTokenValues(address myAddr, address myProxy) public view returns (
-                                                                                            uint blockNumber,
-                                                                                            uint[] gem,
-                                                                                            uint[] gov,
-                                                                                            uint[] skr,
-                                                                                            uint[] sai,
-                                                                                            uint[] sin
-                                                                                        ) {
-        blockNumber = block.number;
+        tValues = new uint[](19);
+        tValues[0] = gem.totalSupply();
+        tValues[1] = gem.balanceOf(myAddr);
+        tValues[2] = gem.balanceOf(tub);
+        tValues[3] = gem.balanceOf(tap);
 
-        gem = new uint[](3);
-        gem[0] = tub.gem().balanceOf(myAddr);
-        gem[1] = tub.gem().balanceOf(tub);
-        gem[2] = tub.gem().balanceOf(tub.tap());
+        tValues[4] = gov.totalSupply();
+        tValues[5] = gov.balanceOf(myAddr);
+        tValues[6] = gov.balanceOf(pit);
+        tValues[7] = gov.allowance(myAddr, myProxy);
 
-        gov = new uint[](3);
-        gov[0] = tub.gov().balanceOf(myAddr);
-        gov[1] = tub.gov().balanceOf(tub.pit());
-        gov[2] = tub.gov().allowance(myAddr, myProxy);
+        tValues[8] = skr.totalSupply();
+        tValues[9] = skr.balanceOf(myAddr);
+        tValues[10] = skr.balanceOf(tub);
+        tValues[11] = skr.balanceOf(tap);
 
-        skr = new uint[](3);
-        skr[0] = tub.skr().balanceOf(myAddr);
-        skr[1] = tub.skr().balanceOf(tub);
-        skr[2] = tub.skr().balanceOf(tub.tap());
+        tValues[12] = sai.totalSupply();
+        tValues[13] = sai.balanceOf(myAddr);
+        tValues[14] = sai.balanceOf(tap);
+        tValues[15] = sai.allowance(myAddr, myProxy);
 
-        sai = new uint[](3);
-        sai[0] = tub.sai().balanceOf(myAddr);
-        sai[1] = tub.sai().balanceOf(tub.tap());
-        sai[2] = tub.sai().allowance(myAddr, myProxy);
-
-        sin = new uint[](2);
-        sin[0] = tub.skr().balanceOf(tub);
-        sin[1] = tub.sin().balanceOf(tub.tap());
+        tValues[16] = sin.totalSupply();
+        tValues[17] = sin.balanceOf(tub);
+        tValues[18] = sin.balanceOf(tap);
     }
 
     function aggregateCDPValues(bytes32 cup) public view returns (
@@ -171,12 +219,12 @@ contract SaiValuesAggregator is DSMath {
         safe = tub.safe(cup);
 
         uint pro = rmul(tub.tag(), r[0]);
-        r[3] = wdiv(pro, rmul(tub.vox().par(), tub.tab(cup)));
+        r[3] = wdiv(pro, rmul(vox.par(), tub.tab(cup)));
         // r[3]: ratio
 
-        r[4] = safe ? sub(rdiv(pro, rmul(tub.mat(), tub.vox().par())), tub.tab(cup)) : 0;
+        r[4] = safe ? sub(rdiv(pro, rmul(tub.mat(), vox.par())), tub.tab(cup)) : 0;
         // r[4]: availDAI
-        uint minSKRNeeded = rdiv(rmul(rmul(tub.tab(cup), tub.mat()), tub.vox().par()), tub.tag());
+        uint minSKRNeeded = rdiv(rmul(rmul(tub.tab(cup), tub.mat()), vox.par()), tub.tag());
         r[5] = safe ? sub(r[0], minSKRNeeded > 0.005 ether ? minSKRNeeded : 0.005 ether) : 0;
         // r[5]: availSKR
         r[6] = rmul(r[5], tub.per());
@@ -184,41 +232,4 @@ contract SaiValuesAggregator is DSMath {
         r[7] = r[0] > 0 && r[1] > 0 ? wdiv(rdiv(rmul(tub.tab(cup), tub.mat()), tub.per()), r[0]) : 0;
         // r[7]: liqPrice
     }
-
-    // // Return just the aggregated tub values
-    // function aggregateTubValues() public view returns (bytes32 pip, bool pipSet, bytes32 pep, bool pepSet, bool off, bool out, uint[] r) {
-
-    //     (pip, pipSet) = tub.pip().peek(); // Price feed value for gem
-    //     (pep, pepSet) = tub.pep().peek(); // Price feed value for gov
-
-    //     off = tub.off(); // Cage flag
-    //     out = tub.out(); // Post cage exit
-
-    //     r = new uint[](12);
-    //     r[0] = tub.axe(); // Liquidation penalty
-    //     r[1] = tub.mat(); // Liquidation ratio
-    //     r[2] = tub.cap(); // Debt ceiling
-    //     r[3] = tub.fit(); // REF per SKR (just before settlement)
-    //     r[4] = tub.tax(); // Stability fee
-    //     r[5] = tub.fee(); // Governance fee
-    //     r[6] = tub.chi(); // Accumulated Tax Rates
-    //     r[7] = tub.rhi(); // Accumulated Tax + Fee Rates
-    //     r[8] = tub.rho(); // Time of last drip
-    //     r[9] = tub.gap(); // Join-Exit Spread
-    //     r[10] = tub.tag(); // Abstracted collateral price (ref per skr)
-    //     r[11] = tub.per(); // Wrapper ratio (gem per skr)
-    // }
-
-    // // Return just the aggregated vox values
-    // function aggregateVoxValues() public view returns (uint par, uint way, uint era) {
-    //     par = tub.vox().par(); // Dai Target Price (ref per dai)
-    //     way = tub.vox().way(); // The holder fee (interest rate)
-    //     era = tub.vox().era();
-    // }
-
-    // // Return just the aggregated tap values
-    // function aggregateTapValues() public view returns (uint fix, uint gap) {
-    //     fix = tub.tap().fix(); // Cage price
-    //     gap = tub.tap().gap(); // Boom-Bust Spread
-    // }
 }
--- src/SaiValuesAggregator.t.sol
+++ src/SaiValuesAggregator.t.sol
@@ -1,21 +1,123 @@
 pragma solidity ^0.4.24;
 
-import "ds-test/test.sol";
+import "sai/sai.t.sol";
 
 import "./SaiValuesAggregator.sol";
 
-contract SaiValuesAggregatorTest is DSTest {
+contract SaiValuesAggregatorTest is SaiTestBase {
     SaiValuesAggregator aggregator;
 
     function setUp() public {
-        aggregator = new SaiValuesAggregator(0x0000000000000000000000000000000000000000);
+        super.setUp();
+        emit log_named_address('top', top);
+        aggregator = new SaiValuesAggregator(top);
     }
 
-    function testFail_basic_sanity() public {
-        assertTrue(false);
+    function testSaiValuesAggregatorContracts() public {
+        assertEq(address(aggregator.top()), address(top));
+        assertEq(address(aggregator.tub()), address(tub));
+        assertEq(address(aggregator.tap()), address(tap));
+        assertEq(address(aggregator.vox()), address(vox));
+        assertEq(address(aggregator.pit()), address(pit));
+        assertEq(address(aggregator.pip()), address(pip));
+        assertEq(address(aggregator.pep()), address(pep));
+        assertEq(address(aggregator.gem()), address(gem));
+        assertEq(address(aggregator.gov()), address(gov));
+        assertEq(address(aggregator.skr()), address(skr));
+        assertEq(address(aggregator.sai()), address(sai));
+        assertEq(address(aggregator.sin()), address(sin));
     }
 
-    function test_basic_sanity() public {
-        assertTrue(true);
+    function testSaiValuesAggregatorGetContracts() public {
+        (
+            ,
+            address top2,
+            address tub2,
+            address tap2,
+            address vox2,
+            address pit2,
+            address pip2,
+            address pep2,
+            address gem2,
+            address gov2,
+            address skr2,
+            address sai2,
+            address sin2
+        ) = aggregator.getContractsAddrs();
+
+        assertEq(top2, address(top));
+        assertEq(tub2, address(tub));
+        assertEq(tap2, address(tap));
+        assertEq(vox2, address(vox));
+        assertEq(pit2, address(pit));
+        assertEq(pip2, address(pip));
+        assertEq(pep2, address(pep));
+        assertEq(gem2, address(gem));
+        assertEq(gov2, address(gov));
+        assertEq(skr2, address(skr));
+        assertEq(sai2, address(sai));
+        assertEq(sin2, address(sin));
+    }
+
+    function testSaiValuesAggregatorGetAggregatedValues() public {
+        (
+            ,
+            bytes32 pipVal,
+            bool pipSet,
+            bytes32 pepVal,
+            bool pepSet,
+            bool off,
+            bool out,
+            uint[] memory sValues,
+            uint[] memory tValues
+        ) = aggregator.aggregateValues(address(this), address(0));
+
+        (bytes32 pipVal2, bool pipSet2) = pip.peek();
+        assertEq(pipVal, pipVal2);
+        assertTrue(pipSet == pipSet2);
+        (bytes32 pepVal2, bool pepSet2) = pip.peek();
+        assertEq(pepVal, pepVal2);
+        assertTrue(pepSet == pepSet2);
+
+        assertTrue(off == tub.off());
+        assertTrue(out == tub.out());
+
+        assertEq(sValues[0], tub.axe());
+        assertEq(sValues[1], tub.mat());
+        assertEq(sValues[2], tub.cap());
+        assertEq(sValues[3], tub.fit());
+        assertEq(sValues[4], tub.tax());
+        assertEq(sValues[5], tub.fee());
+        assertEq(sValues[6], tub.chi());
+        assertEq(sValues[7], tub.rhi());
+        assertEq(sValues[8], tub.rho());
+        assertEq(sValues[9], tub.gap());
+        assertEq(sValues[10], tub.tag());
+        assertEq(sValues[11], tub.per());
+        assertEq(sValues[12], vox.par());
+        assertEq(sValues[13], vox.way());
+        assertEq(sValues[14], vox.era());
+        assertEq(sValues[15], tap.fix());
+        assertEq(sValues[16], tap.gap());
+
+        assertEq(tValues[0], gem.totalSupply());
+        assertEq(tValues[1], gem.balanceOf(address(this)));
+        assertEq(tValues[2], gem.balanceOf(tub));
+        assertEq(tValues[3], gem.balanceOf(tap));
+        assertEq(tValues[4], gov.totalSupply());
+        assertEq(tValues[5], gov.balanceOf(address(this)));
+        assertEq(tValues[6], gov.balanceOf(pit));
+        assertEq(tValues[7], gov.allowance(address(this), address(0)));
+        assertEq(tValues[8], skr.totalSupply());
+        assertEq(tValues[9], skr.balanceOf(address(this)));
+        assertEq(tValues[10], skr.balanceOf(tub));
+        assertEq(tValues[11], skr.balanceOf(tap));
+        assertEq(tValues[12], sai.totalSupply());
+        assertEq(tValues[13], sai.balanceOf(address(this)));
+        assertEq(tValues[14], sai.balanceOf(tap));
+        assertEq(tValues[15], sai.allowance(address(this), address(0)));
+        assertEq(tValues[16], sin.totalSupply());
+        assertEq(tValues[17], sin.balanceOf(tub));
+        assertEq(tValues[18], sin.balanceOf(tap));
     }
 }
