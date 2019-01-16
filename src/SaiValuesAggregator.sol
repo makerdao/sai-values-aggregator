c03fcc18462d9c989d89ab45f8204641280cc4e3
Gonzalo Balabasquer <gbalabasquer@gmail.com>
2019-01-16 17:14:03 -0300

    Add license header

--- src/SaiValuesAggregator.sol
+++ src/SaiValuesAggregator.sol
@@ -1,5 +1,8 @@
 // SaiValuesAggregator.sol -- Sai values aggregator
 
+// Copyright (C) 2018-2019 Michael Elliot <mike@makerdao.com>
+// Copyright (C) 2018-2019 Gonzalo Balabasquer <gbalabasquer@gmail.com>
+//
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
